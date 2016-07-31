bb5739722f470f32d07295da90e2df3912411505
Darryn Soper <darryn.soper@shopify.com>
2016-08-01 11:30:17 +1200

    [diff-read] updateing styles

---
+++ js/main.js
@@ -0,0 +1,201 @@
+$(document).ready(function() {
+  
+  var $diffInput = $('#diff-input');
+  var $diffSubmit = $('#diff-submit');
+  var $diffOutput = $('#diff-output');
+  var regex = /[\n]+/g;
+  
+  $(document).keypress(function(e) {
+    if(e.which === 13) {
+      e.preventDefault();
+      $diffSubmit.click();
+    }
+  });
+  
+  $diffSubmit.on('click', function() {
+    
+    $diffOutput.html('');
+    
+    var diffInputArray = $diffInput.val().split('diff --git ');
+    
+    // Note to self, output as a table as per github
+    // Look for `diff --git` and build a URL/link to the file the store admin
+    
+    
+    // Each block of code
+    $.each(diffInputArray, function(i, blockVal) {
+      
+      if (i > 0) {
+        
+        if (blockVal.indexOf('config.yml') === -1) {
+        
+          var diffBlockArray = blockVal.split(/[\n]+/g);
+          var diffBlockContent = '';
+          var fileEditedMessage;
+          var origIndex;
+          var origIndexStart = 0;
+          var origIndexInc;
+          var newIndex;
+          var newIndexStart = 0;
+          var newIndexInc;
+          var incOrigIndex;
+          var incNewIndex;
+          var editedSection;
+          var editedFile;
+
+
+          // Each line in block
+          $.each(diffBlockArray, function(i, val) {
+
+            // If first line in block, create descriptive sentence
+            if (i === 0) {
+
+              var lineValueArray = val.split('/');
+              var lineValueArrayOffset = lineValueArray.length - 2;
+
+              $.each(lineValueArray, function(i, val) {
+
+                if (i >= lineValueArrayOffset) {
+
+                  // To Do: If new file, update verbiage.
+
+                  if (fileEditedMessage === undefined) {
+                    editedSection = val;
+                    fileEditedMessage = 'Under the <strong>' + editedSection + '</strong> section in the ';
+                  } else {
+                    editedFile = val;
+                    fileEditedMessage = fileEditedMessage + '<strong>' + editedFile + '</strong> file, make the following edits:';
+                  }
+
+                }
+
+              });
+
+            }
+
+            if (i > 1) {
+
+              if (val.indexOf("\ No newline at end of file") === -1) {
+
+                if (val.indexOf("index ") !== 0) {
+
+                  if (val.indexOf("+++") === -1) {
+
+                    if (val.indexOf("---") === -1) {
+
+                      if (val.indexOf("@@") > -1) {
+
+                        var indexRaw = val.replace('@@ ', '').replace(' @@', '');
+                        var indexRawArray = indexRaw.split(' ');
+
+                        $.each(indexRawArray, function(i, lineIndexVal) {
+
+                          if (lineIndexVal.indexOf('-') > -1) {
+                            var origIndexFormat = lineIndexVal.replace('-', '');
+                            var origIndexArray = origIndexFormat.split(',');
+
+                            origIndexStart = parseInt(origIndexArray[0]);
+                            origIndexInc = parseInt(origIndexArray[1]);
+                          }
+
+                          if (lineIndexVal.indexOf('+') > -1) {
+                            var newIndexFormat = lineIndexVal.replace('+', '');
+                            var newIndexArray = newIndexFormat.split(',');
+
+                            newIndexStart = parseInt(newIndexArray[0]);
+                            newIndexInc = parseInt(newIndexArray[1]);
+                          }
+
+                        });
+
+                      } else {
+
+                        var lineValue = val.replace(/</g, '&lt;');
+                        var contentEdited = '';
+                        var contentEditedSymbol = '';
+
+                        if (val.indexOf("+") === 0) {
+                          contentEdited = 'content--added';
+                          contentEditedSymbol = '+';
+                          lineValue = lineValue.replace('+', '');
+                        }
+
+                        if (val.indexOf("-") === 0) {
+                          contentEdited = 'content--removed';
+                          contentEditedSymbol = '-';
+                          lineValue = lineValue.replace('-', '');
+                        }
+
+                        // Contains '-' or nothing
+                        if (val.indexOf("+") !== 0) {
+                          incOrigIndex = true;
+                          if (origIndex) {
+                            origIndex = origIndex + 1;
+                          } else {
+                            origIndex = origIndexStart;
+                          }
+                        } else {
+                          incOrigIndex = false;
+                        }
+
+                        // Contains '+' or nothing
+                        if (val.indexOf("-") !== 0) {
+                          incNewIndex = true;
+                          if (newIndex) {
+                            newIndex = newIndex + 1;
+                          } else {
+                            newIndex = newIndexStart;
+                          }
+                        } else {
+                          incNewIndex = false;
+                        }
+                        
+                        if (incOrigIndex && incNewIndex) {
+                          diffBlockContent = diffBlockContent + '<tr class="' + contentEdited + '"><td data-number-a="' + origIndex + '"></td><td data-number-b="' + newIndex + '"></td><td data-symbol="' + contentEditedSymbol + '"></td><td>' + lineValue + '</td></tr>';
+                        }
+
+                        if (incOrigIndex && !incNewIndex) {
+                          diffBlockContent = diffBlockContent + '<tr class="' + contentEdited + '"><td data-number-a="' + origIndex + '"></td><td data-number-b=""></td><td data-symbol="' + contentEditedSymbol + '"></td><td>' + lineValue + '</td></tr>';
+                        }
+
+                        if (!incOrigIndex && incNewIndex) {
+                          if (newIndex === newIndexStart) {
+                            fileEditedMessage = 'Under the <strong>' + editedSection + '</strong> section create a new file called <strong>' + editedFile + '</strong> and paste the following code into it.'
+                          }
+                          diffBlockContent = diffBlockContent + '<tr class="' + contentEdited + '"><td data-number-a=""></td><td data-number-b="' + newIndex + '"></td><td data-symbol="' + contentEditedSymbol + '"></td><td>' + lineValue + '</td></tr>';
+                        }
+
+                      }
+
+                    }
+
+                  }
+
+                }
+
+              }
+
+            }
+
+          });
+
+          $diffOutput.append(
+            '<h3>' + fileEditedMessage + '</h3>' +
+            '<div class="code-block">' +
+              '<table>' + 
+                diffBlockContent + 
+              '</table>' +
+            '</div>'
+          );
+
+        }
+        
+      }
+      
+    });
+    
+    $diffOutput.prepend('<div class="legend"><div class="grid"><div class="grid__item large--one-half legend--alpha">Add <span class="legend--item legend--add"></span></div><div class="grid__item large--one-half legend--omega"><span class="legend--item legend--remove"></span> Remove</div></div></div>');
+    
+  });
+  
+}); 
